62d963b1c31eeac6e2977e71020b9a46715a785a
sfg4862 <sfg4862@gmail.com>
2025-05-31 18:11:42 +0900

    Hotfix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,5 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
